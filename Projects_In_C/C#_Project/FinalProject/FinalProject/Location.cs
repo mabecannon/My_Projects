18f06af746491acd671bfa500d410f0dd3f647ca
mabecannon <61606775+mabecannon@users.noreply.github.com>
2020-07-02 08:54:38 -0700

    Add files via upload

---
+++ Projects_In_C/C#_Project/FinalProject/FinalProject/Location.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Location
+    {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public Location()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Location(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public override string ToString()
+        {
+            return "X: " + X + " Y: " + Y + "\n";
+        }
+    }
+}
