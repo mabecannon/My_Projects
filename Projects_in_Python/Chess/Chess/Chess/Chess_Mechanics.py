a637d812a2e54072df3655478d51b80dd587d734
mabecannon <61606775+mabecannon@users.noreply.github.com>
2020-08-10 20:35:36 -0700

    Update Chess_Mechanics.py

--- Projects_in_Python/Chess/Chess/Chess/Chess_Mechanics.py
+++ Projects_in_Python/Chess/Chess/Chess/Chess_Mechanics.py
@@ -1,4 +1,17 @@
 import copy
+#Let's start by defining 
+# import only system from os 
+from os import system, name 
+  
+# import sleep to show output for some time period 
+from time import sleep 
+  
+# define our clear function 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
 class piece:
         def __init__(self, name, posx, posy, col):
             self.name = name
@@ -22,110 +35,120 @@ class piece:
         def ToString(self):
             print(name)
 
-master_Game_Board = {
-'a':{ '1': piece('r', 'a', '1', 'w'),  '2':piece('p', 'a', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'a', '7', 'b'), '8': piece('r', 'a', '8', 'b')},
-'b':{ '1': piece('n', 'b', '1', 'w'),  '2':piece('p', 'b', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'b', '7', 'b'), '8': piece('n', 'b', '8', 'b')}, 
-'c':{ '1': piece('b', 'c', '1', 'w'),  '2':piece('p', 'c', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'c', '7', 'b'), '8': piece('b', 'c', '8', 'b')}, 
-'d':{ '1': piece('q', 'd', '1', 'w'),  '2':piece('p', 'd', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'd', '7', 'b'), '8': piece('k', 'd', '8', 'b')},
-'e':{ '1': piece('k', 'e', '1', 'w'),  '2':piece('p', 'e', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'e', '7', 'b'), '8': piece('q', 'e', '8', 'b')},
-'f':{ '1': piece('b', 'f', '1', 'w'),  '2':piece('p', 'f', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'f', '7', 'b'), '8': piece('b', 'f', '8', 'b')},
-'g':{ '1': piece('n', 'g', '1', 'w'),  '2':piece('p', 'g', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'g', '7', 'b'), '8': piece('n', 'g', '8', 'b')},
-'h':{ '1': piece('r', 'h', '1', 'w'),  '2':piece('p', 'h', '2', 'w'), '3': '', '4': '', '5': '', '6': '', '7': piece('p', 'h', '7', 'b'), '8': piece('r', 'h', '8', 'b')}}
+master_Game_Board = [
+[ piece('r', 'a', '1', 'w'),  piece('p', 'a', '2', 'w'), '',  '',  '', '', piece('p', 'a', '7', 'b'), piece('r', 'a', '8', 'b')],
+[ piece('n', 'b', '1', 'w'),  piece('p', 'b', '2', 'w'), '',  '',  '', '', piece('p', 'b', '7', 'b'), piece('n', 'b', '8', 'b')], 
+[ piece('b', 'c', '1', 'w'),  piece('p', 'c', '2', 'w'), '',  '',  '', '', piece('p', 'c', '7', 'b'), piece('b', 'c', '8', 'b')], 
+[ piece('q', 'd', '1', 'w'),  piece('p', 'd', '2', 'w'), '',  '',  '', '', piece('p', 'd', '7', 'b'), piece('k', 'd', '8', 'b')],
+[ piece('k', 'e', '1', 'w'),  piece('p', 'e', '2', 'w'), '',  '',  '', '', piece('p', 'e', '7', 'b'), piece('q', 'e', '8', 'b')],
+[ piece('b', 'f', '1', 'w'),  piece('p', 'f', '2', 'w'), '',  '',  '', '', piece('p', 'f', '7', 'b'), piece('b', 'f', '8', 'b')],
+[ piece('n', 'g', '1', 'w'),  piece('p', 'g', '2', 'w'), '',  '',  '', '', piece('p', 'g', '7', 'b'), piece('n', 'g', '8', 'b')],
+[ piece('r', 'h', '1', 'w'),  piece('p', 'h', '2', 'w'), '',  '',  '', '', piece('p', 'h', '7', 'b'), piece('r', 'h', '8', 'b')]]
+#This is a standard board tilted 90deg to the right from the perspective of black (or 90deg left for white)
+                                                                                                                                                                          #[x = letter][y = number]  
 
-for x in master_Game_Board:
-    for y in x: 
-        if y == '':
-            y = piece('', x, y, '')# Fill the game board with empty pieces, will be useful for determining move legality
+for x in range(len(master_Game_Board)):
+    for y in range(len(master_Game_Board[x])): 
+        if master_Game_Board[x][y] == '':
+            master_Game_Board[x][y] = piece('', x, y, '')# Fill the game board with empty pieces, will be useful for determining move legality
 
 
 new_Game = copy.deepcopy(master_Game_Board)# start a new game from the master template
 
+    #With a completed game board, let's define how these pieces can move around the board
+    #The board (array) contains info about where the pieces are and so do the pieces themselves
+    #A decoder function will make it easier to check the validity of user or AI inputs on pieces of the board
+    #Let's do the decoder next!    
+def decode_Piece_X(x):
+     if x == 'a':
+        return 1
+     else: 
+        if x == 'b':
+            return 2
+        else:
+            if x == 'c':
+               return 3
+            else:
+                if x == 'd':
+                   return 4
+                else:
+                    if x == 'e':
+                       return 5
+                    else:
+                        if x == 'f':
+                            return 6
+                        else:
+                            if x == 'g':
+                               return 7
+                            else:
+                                if x == 'h':
+                                   return 8
+        
+def decode_Piece_Y(y):
+    y = int(y)
+    print(type(y))
+    return y
+
+
+
+
+#Ok let's make a basic move function so that we can use it test some basic validity, thus testing the fundamental structure of the game thus far
 def Move(oldX, oldY, newX, newY):
     if new_Game[newX][newY].getPiece() != '':
         if new_Game[newX][newY].getColor() == new_Game[oldX][oldY].getColor(): # return false since you can't have two pieces of the same color on the same square
             return False
         else:
             print("Wow, you took a " + new_Game[newX][newY].getPiece())
-    del new_Game[newX][newY]
+    #every move creates an empty space in the old position, regardless of if a piece is captured or not, and the board must be updated    
     new_Game[oldX][oldY].setPosX(newX)
     new_Game[oldX][oldY].setPosY(newY)
     new_Game[newX][newY] = new_Game[oldX][oldY]
     new_Game[oldX][oldY] = piece('', oldX, oldY, '')
     return True
 
-def ValidMove(piece, x, y, xP, yP):
-    if x == 'a':
-        k = 1
-    else: 
-        if x == 'b':
-            k = 2
-        else:
-            if x == 'c':
-                k = 3
-            else:
-                if x == 'd':
-                    k = 4
-                else:
-                    if x == 'e':
-                        k = 5
-                    else:
-                        if x == 'f':
-                            k = 6
-                        else:
-                            if x == 'g':
-                                k = 7
-                            else:
-                                if x == 'h':
-                                    k = 8
-    
-                                    
-    if xP == 'a':
-        kP = 1
-    else: 
-        if xP == 'b':
-            kP = 2
-        else:
-            if xP == 'c':
-                kP = 3
-            else:
-                if xP == 'd':
-                    kP = 4
-                else:
-                    if xP == 'e':
-                        kP = 5
-                    else:
-                        if xP == 'f':
-                            kP = 6
-                        else:
-                            if xP == 'g':
-                                kP = 7
-                            else:
-                                if xP == 'h':
-                                    kP = 8
-    #add decoder for y values; strings are no good for comparing
-    if new_Game[x][y].getPiece() == 'p' and  new_Game[x][y].getColor() == 'w': # valid moves for pawns
-            if yP - y == 1:
-                if (kP - k == 0 and new_Game[xP][yP].getPiece() == '') or (abs(kP - k) == 1 and new_Game[xP][yP].getPiece() != ''):
-                    if yP == 8:
+
+
+
+
+#Let's see what it looks like! What a headache to just try to remember and find where all the pieces are!
+def PrintBoard():
+  
+        for y in range(len(new_Game)):
+            print(new_Game[0][y].getPiece(), ' ', new_Game[1][y].getPiece(), ' ', new_Game[2][y].getPiece(), ' ', new_Game[3][y].getPiece(), ' ', new_Game[4][y].getPiece(), ' ', new_Game[5][y].getPiece(), ' ', new_Game[6][y].getPiece(), ' ', new_Game[7][y].getPiece(), ' ' )
+        
+
+
+
+
+#Let's just test a pawn since they have a rather complex moveset which can be representative of other sophisticated moves like castling 
+def ValidMove(x, y, xNew, yNew):
+   if new_Game[x][y].getPiece() == 'p' and new_Game[x][y].getColor() == 'w': # valid moves for white pawns
+            if int(yNew) - int(y) == 1: #white pawns can only move in the positive y direction 1 at a time 
+                if (xNew - x == 0 and new_Game[xNew][yNew].getPiece() == '') or (abs(xNew - x) == 1 and new_Game[xNew][yNew].getPiece() != ''): #pawns can only have an x value iff they are attacking a piece
+                                                                                                                                    #if they aren't attacking, they cannot have an x value
+                                                                                                                                    #thus a pawn cannot move forward if there is a piece in the way
+                                                                                                                #It can however if there is a piece on either side of its foward path    
+                    if yNew == 8:
                         new_Game[x][y].setPiece('q')
-                    return Move(x, y, xP, yP)
+                    return Move(x, y, xNew, yNew)
                 else: 
                     return False
             else: 
-                if yP - y == 2 and y == 2 and kP - k == 0 and new_Game[xP][yP].getPiece() == '' and new_Game[xP][yP - 1].getPiece() == '':
-                    return Move(x, y, xP, yP)
+                if yNew - y == 2 and y == 2 and kNew - k == 0 and new_Game[xNew][yNew].getPiece() == '' and new_Game[xNew][yNew - 1].getPiece() == '':
+                    return Move(x, y, xNew, yNew)
                 else:
                     return False
 
 
 
-
-
-print(ValidMove('p', 'a', '2', 'a', '3'))
-print(master_Game_Board['a']['2'].getPiece())
-print(master_Game_Board['a']['2'].getPosX())
-print(master_Game_Board['a']['2'].getPosY())
-print(master_Game_Board['a']['7'].getPiece())
-print(master_Game_Board['a']['7'].getPosX())
-print(master_Game_Board['a']['7'].getPosY())
+PrintBoard()
+print(ValidMove(0, 1, 0, 2))
+clear()
+PrintBoard()
+#print(master_Game_Board['a']['2'].getPiece())
+#print(master_Game_Board['a']['2'].getPosX())
+#print(master_Game_Board['a']['2'].getPosY())
+#print(master_Game_Board['a']['7'].getPiece())
+#print(master_Game_Board['a']['7'].getPosX())
+#print(master_Game_Board['a']['7'].getPosY())
   
